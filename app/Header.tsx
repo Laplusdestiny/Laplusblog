da630b8c6792fcd990bf02e1b7961dd2d6f8ba04
Laplusdestiny <prayonshootingstars@gmail.com>
2024-11-14 02:34:56 +0000

    スタイルの調整: ヘッダーとコンテンツのマージン、パディング、フォントサイズを更新

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header className="py-5 px-10 border-b flex justify-between">
-            <div>
-                <h1>
+        <header className="py-5 px-10 border-b flex justify-between items-center text-black">
+            <div className="flex items-center">
+                <h1 className="text-2xl font-bold">
                     <Link href="/">Laplusblog</Link>
                 </h1>
             </div>
