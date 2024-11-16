4cea9a30bd3ded7886bd3780b45b6e7629e5a1fa
Laplusdestiny <prayonshootingstars@gmail.com>
2024-11-16 10:34:38 +0000

    404および500エラーページを追加し、マークダウンファイルの存在を確認する機能を実装。バージョンを0.1.2に更新。

---
+++ app/error.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function ErrorPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-gray-800">500 - Page Not Found</h1>
+        </div>
+    );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client";
+export default function NotFound() {
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+                <h1 className="text-4xl font-bold text-gray-800">404 - Page Not Found</h1>
+            </div>
+        </>
+    );
+}
