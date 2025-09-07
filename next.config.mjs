cc22075d0bd415e6c0498f34455b211618b007b6
Laplusdestiny <laplusdestiny@laplusdestiny.com>
2025-09-07 09:53:40 +0000

    chore: update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig = {
     images: {
@@ -25,8 +24,5 @@ const nextConfig = {
     },
 };
 
-if (process.env.NODE_ENV === 'development') {
-    await setupDevPlatform();
-}
 
 export default nextConfig;
